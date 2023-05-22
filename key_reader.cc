6e8441afdf14d3f646116321112e548075553a5c
Balasubramanian Azhagappan <balasubramanian.azhagappan@bosch.com>
2023-05-22 18:07:15 +0200

    a working version for signing payload using a key from file.

--- key_reader.cc
+++ key_reader.cc
@@ -12,66 +12,10 @@
 #include <openssl/evp.h>
 #include <openssl/decoder.h>
 #include <openssl/pem.h>
+#include <openssl/param_build.h>
+#include <openssl/ec.h>
 
-EC_KEY* ecKey = nullptr;
-EVP_PKEY* pkey = nullptr;
-
-void setPrivateKeyFromPEM(const std::string& pemkey)
-{
-    pkey = EVP_PKEY_new();
-
-    BIO* bio = BIO_new(BIO_s_mem());
-
-    int bio_write_ret = BIO_write(
-        bio, static_cast<const char*>(pemkey.c_str()), pemkey.size());
-    if (bio_write_ret <= 0) {
-        throw std::runtime_error("error1");
-    }
-
-    if (!PEM_read_bio_PrivateKey(bio, &pkey, NULL, NULL)) {
-        throw std::runtime_error("error1.5");
-    }
-
-    EC_KEY* eckey_local = EVP_PKEY_get1_EC_KEY(pkey);
-
-    if (!eckey_local) {
-        throw std::runtime_error("error2");
-    } else {
-        ecKey = eckey_local;
-        EC_KEY_set_asn1_flag(ecKey, OPENSSL_EC_NAMED_CURVE);
-    }
-}
-
-std::string getPrivateKeyAsPEM()
-{
-    if (!pkey) {
-        throw std::runtime_error("error3");
-    }
-
-    BIO* outbio = BIO_new(BIO_s_mem());
-
-    if (!PEM_write_bio_ECPrivateKey(outbio, ecKey, NULL, NULL, 0, 0,
-                                  NULL)) {
-        throw std::runtime_error("error4");
-    }
-
-    std::string keyStr;
-    int         priKeyLen = BIO_pending(outbio);
-    keyStr.resize(priKeyLen);
-    BIO_read(outbio, (void*)&(keyStr.front()), priKeyLen);
-    return keyStr;
-}
-
-int main()
-{
-    std::string expectedPrivKey =
-        "-----BEGIN EC PRIVATE KEY-----\n"
-        "MIGkAgEBBDBNK0jwKqqf8zkM+Z2l++9r8bzdTS/XCoB4N1J07dPxpByyJyGbhvIy\n"
-        "1kLvY2gIvlmgBwYFK4EEACKhZANiAAQvPxAK2RhvH/k5inDa9oMxUZPvvb9fq8G3\n"
-        "9dKW1tS+ywhejnKeu/48HXAXgx2g6qMJjEPpcTy/DaYm12r3GTaRzOBQmxSItStk\n"
-        "lpQg5vf23Fc9fFrQ9AnQKrb1dgTkoxQ=\n"
-        "-----END EC PRIVATE KEY-----\n";
-
+int main() {
     std::ifstream key_file;
 
     key_file.open("/tmp/private.key", std::iostream::in);
@@ -82,16 +26,100 @@ int main()
         key << key_file.rdbuf();
         const std::string& private_key = key.str();
 
-        setPrivateKeyFromPEM(expectedPrivKey);
-        // compare priv key
-        {
-            std::string privKeyRead = getPrivateKeyAsPEM();
-            std::cout << privKeyRead << std::endl;
-            std::cout<<expectedPrivKey<<std::endl;
+        std::cout << "Using private key (" << private_key.size() << " bytes): " << std::endl << private_key << std::endl;
+
+        //load key
+        EVP_PKEY *pkey = EVP_PKEY_new();
+
+        BIO* bio = BIO_new(BIO_s_mem());
+
+        int bio_write_ret = BIO_write(
+            bio, static_cast<const char*>(private_key.c_str()), private_key.size());
+        if (bio_write_ret <= 0) {
+            throw std::runtime_error("error1");
         }
+
+        if (!PEM_read_bio_PrivateKey(bio, &pkey, NULL, NULL)) {
+            throw std::runtime_error("error1.5");
+        }
+        BIO_free(bio);
+        //load key done
+
+        OSSL_LIB_CTX *libctx = NULL;
+        const char *sig_name = "SHA-256";
+        size_t sig_len = 0;
+        unsigned char *sig_value = NULL;
+
+        const char *payload = "Hello World";
+
+        libctx = OSSL_LIB_CTX_new();
+        if (libctx == NULL) {
+            fprintf(stderr, "OSSL_LIB_CTX_new() returned NULL\n");
+        }
+
+        EVP_MD_CTX *sign_context = NULL;
+
+        /*
+         * Make a message signature context to hold temporary state
+         * during signature creation
+         */
+        sign_context = EVP_MD_CTX_new();
+        if (sign_context == NULL) {
+            fprintf(stderr, "EVP_MD_CTX_new failed.\n");
+            //goto cleanup;
+        }
+        /*
+         * Initialize the sign context to use the fetched
+         * sign provider.
+         */
+        if (!EVP_DigestSignInit_ex(sign_context, NULL, sig_name,
+                                  libctx, NULL, pkey, NULL)) {
+            fprintf(stderr, "EVP_DigestSignInit_ex failed.\n");
+            //goto cleanup;
+        }
+        /*
+         * EVP_DigestSignUpdate() can be called several times on the same context
+         * to include additional data.
+         */
+        if (!EVP_DigestSignUpdate(sign_context, payload, strlen(payload))) {
+            fprintf(stderr, "EVP_DigestSignUpdate(hamlet_1) failed.\n");
+            //goto cleanup;
+        }
+        /* Call EVP_DigestSignFinal to get signature length sig_len */
+        if (!EVP_DigestSignFinal(sign_context, NULL, &sig_len)) {
+            fprintf(stderr, "EVP_DigestSignFinal failed.\n");
+            //goto cleanup;
+        }
+        if (sig_len <= 0) {
+            fprintf(stderr, "EVP_DigestSignFinal returned invalid signature length.\n");
+            //goto cleanup;
+        }
+        sig_value = (unsigned char*)OPENSSL_malloc(sig_len);
+        if (sig_value == NULL) {
+            fprintf(stderr, "No memory.\n");
+            //goto cleanup;
+        }
+        if (!EVP_DigestSignFinal(sign_context, sig_value, &sig_len)) {
+            fprintf(stderr, "EVP_DigestSignFinal failed.\n");
+            //goto cleanup;
+        }
+
+        fprintf(stdout, "Generating signature:\n");
+        BIO_dump_indent_fp(stdout, sig_value, sig_len, 2);
+        fprintf(stdout, "\n");
+
+
+        std::cout << "signature: " << sig_value << std::endl;
+        std::cout << "signature length: " << sig_len << std::endl;
+
+
+        OSSL_LIB_CTX_free(libctx);
+        OPENSSL_free(sig_value);
     } else {
         std::cout << "Couldn't open private key" << std::endl;
+        return 1;
     }
+
 
     return 0;
 }
