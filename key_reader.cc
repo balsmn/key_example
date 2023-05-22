ff2fe8c3aac4f12e321d3ffae3109ade0333a1e6
Balasubramanian Azhagappan <balasubramanian.azhagappan@bosch.com>
2023-05-22 18:31:37 +0200

    added cleanup function to cleanup before exiting

--- key_reader.cc
+++ key_reader.cc
@@ -16,9 +16,17 @@
 #include <openssl/ec.h>
 
 int main() {
+    int ret = 0;
     std::ifstream key_file;
+    EVP_PKEY *pkey = EVP_PKEY_new();
+    OSSL_LIB_CTX *libctx = NULL;
+    EVP_MD_CTX *sign_context = NULL;
+    unsigned char *sig_value = NULL;
+    const char *sig_name = "SHA-256";
+    size_t sig_len = 0;
+    const char *payload = "Hello World";
 
-    key_file.open("/tmp/private.key", std::iostream::in);
+    key_file.open("/data/encrypted/crt/key/key_0.pem", std::iostream::in);
 
     if(key_file.is_open()) {
         //transform key
@@ -29,8 +37,6 @@ int main() {
         std::cout << "Using private key (" << private_key.size() << " bytes): " << std::endl << private_key << std::endl;
 
         //load key
-        EVP_PKEY *pkey = EVP_PKEY_new();
-
         BIO* bio = BIO_new(BIO_s_mem());
 
         int bio_write_ret = BIO_write(
@@ -43,21 +49,12 @@ int main() {
             throw std::runtime_error("error1.5");
         }
         BIO_free(bio);
-        //load key done
-
-        OSSL_LIB_CTX *libctx = NULL;
-        const char *sig_name = "SHA-256";
-        size_t sig_len = 0;
-        unsigned char *sig_value = NULL;
-
-        const char *payload = "Hello World";
+        //loading private key done
 
         libctx = OSSL_LIB_CTX_new();
         if (libctx == NULL) {
             fprintf(stderr, "OSSL_LIB_CTX_new() returned NULL\n");
         }
-
-        EVP_MD_CTX *sign_context = NULL;
 
         /*
          * Make a message signature context to hold temporary state
@@ -66,7 +63,7 @@ int main() {
         sign_context = EVP_MD_CTX_new();
         if (sign_context == NULL) {
             fprintf(stderr, "EVP_MD_CTX_new failed.\n");
-            //goto cleanup;
+            goto cleanup;
         }
         /*
          * Initialize the sign context to use the fetched
@@ -75,7 +72,7 @@ int main() {
         if (!EVP_DigestSignInit_ex(sign_context, NULL, sig_name,
                                   libctx, NULL, pkey, NULL)) {
             fprintf(stderr, "EVP_DigestSignInit_ex failed.\n");
-            //goto cleanup;
+            goto cleanup;
         }
         /*
          * EVP_DigestSignUpdate() can be called several times on the same context
@@ -83,43 +80,43 @@ int main() {
          */
         if (!EVP_DigestSignUpdate(sign_context, payload, strlen(payload))) {
             fprintf(stderr, "EVP_DigestSignUpdate(hamlet_1) failed.\n");
-            //goto cleanup;
+            goto cleanup;
         }
         /* Call EVP_DigestSignFinal to get signature length sig_len */
         if (!EVP_DigestSignFinal(sign_context, NULL, &sig_len)) {
             fprintf(stderr, "EVP_DigestSignFinal failed.\n");
-            //goto cleanup;
+            goto cleanup;
         }
         if (sig_len <= 0) {
             fprintf(stderr, "EVP_DigestSignFinal returned invalid signature length.\n");
-            //goto cleanup;
+            goto cleanup;
         }
         sig_value = (unsigned char*)OPENSSL_malloc(sig_len);
         if (sig_value == NULL) {
             fprintf(stderr, "No memory.\n");
-            //goto cleanup;
+            goto cleanup;
         }
         if (!EVP_DigestSignFinal(sign_context, sig_value, &sig_len)) {
             fprintf(stderr, "EVP_DigestSignFinal failed.\n");
-            //goto cleanup;
+            goto cleanup;
         }
 
         fprintf(stdout, "Generating signature:\n");
         BIO_dump_indent_fp(stdout, sig_value, sig_len, 2);
         fprintf(stdout, "\n");
 
-
         std::cout << "signature: " << sig_value << std::endl;
         std::cout << "signature length: " << sig_len << std::endl;
-
-
-        OSSL_LIB_CTX_free(libctx);
-        OPENSSL_free(sig_value);
+        return ret;
     } else {
         std::cout << "Couldn't open private key" << std::endl;
-        return 1;
+        return ret;
     }
+cleanup:
+        OPENSSL_free(sig_value);
+        EVP_MD_CTX_free(sign_context);
+        OSSL_LIB_CTX_free(libctx);
+        EVP_PKEY_free(pkey);
 
-
-    return 0;
+        return ret;
 }
